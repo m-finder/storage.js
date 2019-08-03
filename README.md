2053d37f7f0de970f96af7a1b4c4d6ec6f1b3d44
M-finder <m@m-finder.com>
2019-08-03 15:19:42 +0800

    create project

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# storage.js
-modules for localStorage
+# <center>storage.js</center>
+
+localStorage modules for laravel
+
+#### use
+require in app.js
+
+```vue
+window.storage = require('./storage');
+```
+
+```vue
+// save
+storage.set({ id: 1, name: 'm-finder' })
+
+// get
+storage.get('id')
+
+// remove key
+storage.remove('id')
+
+// remove all
+storage.remove()
+```
---
+++ storage.js
@@ -0,0 +1,43 @@
+const storage = window.localStorage;
+const table = 'laravel-storage';
+
+function set(settings) {
+    if (!window.JSON || !window.JSON.parse) return
+
+    settings = typeof settings === 'object'
+        ? settings
+        : {key: settings}
+
+    const data = storage[table] ? JSON.parse(storage[table]) : {}
+
+    if ('value' in settings) {
+        data[settings.key] = settings.value
+    } else {
+        Object.keys(settings).forEach(function (item, index) {
+            data[item] = settings[item]
+        })
+    }
+    storage[table] = JSON.stringify(data)
+}
+
+function get(key) {
+    const data = JSON.parse(storage.getItem(table))
+    try {
+        return data[key]
+    } catch (e) {
+        return null
+    }
+}
+
+function remove(key = null) {
+    if (key === null) {
+        storage.removeItem(table)
+    } else {
+        const data = JSON.parse(storage[table])
+        delete data[key]
+    }
+}
+
+export {
+    set, get, remove
+}
